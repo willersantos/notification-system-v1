4c8cd5e8a05927e2f954f89dd4ddacb7265d9c13
Pedro Teles <pedroptelez@gmail.com>
2024-05-04 17:57:15 -0300

    :recycle: testing

--- Producer/src/app.service.ts
+++ Producer/src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
     public serverIsRunning(): string {
+        // TODO: Implement this method
         return "Server is running!";
     }
 }
